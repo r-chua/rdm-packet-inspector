8ddd67b4ed59348738201671ff6d7f836ffc789d
Ryan Chua <rchua608@gmail.com>
2026-05-28 16:22:42 -0500

    chore: prettify merged files

--- src/parser/example.test.ts
+++ src/parser/example.test.ts
@@ -1,7 +1,7 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect } from 'vitest';
 
 describe('vitest setup', () => {
   it('works', () => {
-    expect(1 + 1).toBe(2)
-  })
-})
+    expect(1 + 1).toBe(2);
+  });
+});
