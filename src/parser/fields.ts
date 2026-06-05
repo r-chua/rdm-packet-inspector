032628990d7917b228e64b22ac571201bbb1b7d0
Ryan Chua <rchua608@gmail.com>
2026-06-05 14:47:33 -0500

    test: getFieldEntries

--- src/parser/fields.ts
+++ src/parser/fields.ts
@@ -1,6 +1,10 @@
-import type { RdmField } from './types';
+import type { RdmField, RdmPacket } from './types';
 
 export type FieldEntry = {
   name: string;
   field: RdmField<unknown>;
+};
+
+export const getFieldEntries = (packet: RdmPacket): FieldEntry[] => {
+  throw new Error('getFieldEntries is not implemented yet');
 };
