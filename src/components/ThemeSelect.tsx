18f91c0b41d9aa34b77b198ff6944161f981726f
Ryan Chua <rchua608@gmail.com>
2026-08-04 10:42:08 -0500

    refactor: adjust select padding

--- src/components/ThemeSelect.tsx
+++ src/components/ThemeSelect.tsx
@@ -60,8 +60,7 @@ export function ThemeSelect() {
       <select
         id="theme-select"
         className={cn(
-          'px-4',
-          'mt-1 rounded-md',
+          'p-1 mt-1 rounded-md',
           'border border-border shadow-sm sm:text-sm',
           'focus:border-focus focus:ring-focus'
         )}
